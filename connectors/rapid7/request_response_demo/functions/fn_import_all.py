c9a897df9dbf49224671f31fcb8fcb9a056655b2
Shane Curtin <shane_curtin@rapid7.com>
2025-07-29 09:51:24 -0400

    {Feedback} Be more explicit about exit conditions

--- connectors/rapid7/request_response_demo/functions/fn_import_all.py
+++ connectors/rapid7/request_response_demo/functions/fn_import_all.py
@@ -22,6 +22,11 @@ def _get_asset(
         # Get some assets from the client
         r = api_method(current_page)
 
+        # If we don't have a response or the response doesn't contain data, we log a warning and break
+        if not r or not r.get("data") or not r.get("page"):
+            user_log.warning("No data found in response for page %d. Response: %s", current_page, r)
+            break
+
         data = r.get("data", [])
 
         if data:
@@ -37,16 +42,20 @@ def _get_asset(
 
                 yield surcom_type(asset)
 
+        # We check if we have reached the last page
         if r.get("page") == r.get("total_pages"):
             user_log.debug("Reached the last page: %d", current_page)
             break
+
+        # If the total pages setting is > 0, we check if we have reached it
         elif settings.get("total_pages") and r.get("page") >= settings.get("total_pages"):
             user_log.info("Not getting any more pages")
             break
-        else:
-            next_page = current_page + 1
-            user_log.debug("Moving to the next page: %d", next_page)
-            current_page = next_page
+
+        # Else, we increment the page number and continue
+        next_page = current_page + 1
+        user_log.debug("Moving to the next page: %d", next_page)
+        current_page = next_page
 
 
 def import_all(
